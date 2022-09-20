c11f2c55e80f7c5bc0fccc949fe88e2f78cfc925
mmirwaldt <michael.mirwaldt@gmail.com>
2022-09-20 02:41:47 +0200

    improved Example_06_allMatch

--- src/main/java/net/mirwaldt/streams/Example_06_allMatch.java
+++ src/main/java/net/mirwaldt/streams/Example_06_allMatch.java
@@ -13,20 +13,20 @@ import java.util.List;
 
 public class Example_06_allMatch {
     public static void main(String[] args) {
-        var numbersAsStrings = List.of("1", "2", "3", "5", "6", "8", "11");
-        System.out.println(String.join(" + ", numbersAsStrings) + " = " + parseAndSum(numbersAsStrings));
+        var intsAsStrings = List.of("1", "2", "3", "5", "6", "8", "11");
+        System.out.println(String.join(" + ", intsAsStrings) + " = " + parseAndSumPositiveInts(intsAsStrings));
     }
 
-    public static int parseAndSum(List<String> numbersAsStrings) {
-        if (numbersAsStrings.stream().allMatch(str -> str.matches("-?\\d+"))) {
-            return numbersAsStrings.stream()
+    public static int parseAndSumPositiveInts(List<String> intsAsStrings) {
+        if (intsAsStrings.stream().allMatch(str -> str.matches("\\d+"))) {
+            return intsAsStrings.stream()
                     .mapToInt(Integer::parseInt)
                     .sum();
         } else {
-            String nonInt = numbersAsStrings.stream()
-                    .filter(str -> !str.matches("-?\\d+"))
+            String nonInt = intsAsStrings.stream()
+                    .filter(str -> !str.matches("\\d+"))
                     .findFirst().get();
-            throw new IllegalArgumentException("'" + nonInt + "' is not an integer.");
+            throw new IllegalArgumentException("'" + nonInt + "' is not a positive integer.");
         }
     }
 }
